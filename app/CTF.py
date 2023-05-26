8fbb4b4505b5df425fa31c7cb681414a1bc741fe
Alexandre Ondet <alexandre.ondet63@gmail.com>
2023-05-26 14:52:44 -0400

    add Scoreboard to CTF

--- app/CTF.py
+++ app/CTF.py
@@ -2,6 +2,7 @@ import datetime
 from typing import List
 
 from app.Challenge import Challenge
+from app.Scoreboard import Scoreboard
 
 
 class CTF:
@@ -9,12 +10,14 @@ class CTF:
     start: datetime
     end: datetime
     challenges: List[Challenge]
+    scoreboard: Scoreboard
 
     def __init__(self, name, start=datetime.now(), end=datetime.now() + datetime.timedelta(days=1), challenges=[]):
         self.name = name
         self.start = start
         self.end = end
         self.challenges = challenges
+        self.scoreboard = Scoreboard()
 
     def add_challenge(self, challenge: Challenge):
         self.challenges.append(challenge)
